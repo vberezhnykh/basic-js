a285e0f9dc319dd5e44fb1c9d5571a83d389d165
Valentin Berezhnykh <vberezhnykh74@gmail.com>
2022-09-12 17:57:00 +0300

    feat: add solution to task simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,27 +5,43 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  chain: '',
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    if (this.chain.length === 0) this.chain = `( ${(value)} )`;
+    else this.chain += `~~( ${value} )`;
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    let arr = this.chain.split('~~');
+    if (typeof position !== 'number' || 
+        position < 1 ||
+        !Number.isInteger(position) ||
+        arr.length < position) {
+          this.finishChain();
+          throw new Error("You can't remove incorrect link!");
+        } 
+    if (position === 1) arr.shift();
+    else if (position === arr.length) arr.pop();
+    else arr = [...arr.slice(0, position - 1), ...arr.slice(position)];
+    this.chain = arr.join('~~');
+    return this;
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const arr = this.chain.split('~~');
+    this.chain = arr.reverse().join('~~');
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const chain = this.chain;
+    this.chain = '';
+    return chain;
   }
 };
+
+console.log(chainMaker.reverseChain().addLink('ABC').reverseChain().reverseChain().reverseChain().addLink(Infinity).addLink(false).addLink(0).addLink('8.963').removeLink(2).removeLink(1)/* .reverseChain() */.finishChain());
 
 module.exports = {
   chainMaker
